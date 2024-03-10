703ad88b11bf567ddbf6de73e2d7b6d518945fb5
ZurabKing <Mr.BlooD006@gmail.com>
2024-03-10 21:26:50 +0300

    fixed observer

--- src/pages/Category/AllProduct/Products.tsx
+++ src/pages/Category/AllProduct/Products.tsx
@@ -3,10 +3,12 @@ import { observer } from "mobx-react-lite";
 import { useStore } from "../../../hooks/useStore";
 
 import styles from "./Products.module.css";
+import { Posts } from "../../../@types/products";
 
 export const Products: FC = observer(() => {
   const {
     post: { getAllProducts, posts },
+    cart: { addItem },
   } = useStore();
 
   useEffect(() => {
@@ -21,6 +23,10 @@ export const Products: FC = observer(() => {
     return <span>error...</span>;
   }
 
+  const handleAddToCart = (post: Posts) => {
+    addItem(post);
+  };
+
   return (
     <>
       <h1 style={{ marginBottom: "30px" }}>Все товары</h1>
@@ -32,9 +38,9 @@ export const Products: FC = observer(() => {
               <p className={styles.text}>{post.title}</p>
               <div className={styles.btnContainer}>
                 <span>
-                  <b>Цена:</b> ${post.price}{" "}
+                  <b>Цена:</b> ${post.price}
                 </span>
-                <button>В корзину</button>
+                <button onClick={() => handleAddToCart(post)}>В корзину</button>
               </div>
             </li>
           );
